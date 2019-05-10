b6efeec3bb2c4a5c7fca3e2f1a9ba933fb7d0955
catoostrophe <catodekruif@gmail.com>
2019-05-10 09:13:11 +0800

    création Morpion contre robot
    un seul joueur
    meilleure boucle de sortie

--- Morpion vs AI.py
+++ Morpion vs AI.py
@@ -1,4 +1,7 @@
-import sys
+import random,time
+
+listeJ2=[]
+
 
 #Liste des valeurs du tableau:
 choix = []
@@ -6,6 +9,10 @@ choix = []
 #La liste représentant le tableau prends les valeurs de 0 à 8 (qui représentent alors les cases du tableau).
 for x in range (0, 9):
     choix.append(x)
+    listeJ2.append(x)
+
+
+
 
 #Fonction permettant l'affichage du tableau qui sera mis à jour après chaque tour
 def tableau():
@@ -25,6 +32,7 @@ def tourJ1():
         for x in range (0,9): #Le numéro de case choisi par le joueur 1 sera remplacé par "X" si c'est un chiffre compris entre 0 et 8 (9-1).
             if choix[J1]==x:
                 choix[J1]="X"
+                listeJ2.remove(x)
     else: #Lorsque le chiffre saisi n'est pas un chiffre/nombre compris entre 0 et 8 où qu'il est déjà pris, le joueur 1 devra rejouer.
         print("")
         print("Pas possible. Saisissez un chiffre entre 0 et 8 qui n'est pas déjà pris.")
@@ -32,35 +40,45 @@ def tourJ1():
         tourJ1()
 
 def tourJ2(): #Similaire à la fonction tourJ1()
-    J2=int(input("Joueur 2: "))
-    if J2 in choix:
-        for x in range (0,9):
-          if choix[J2]==x:
+    J2 = random.choice(listeJ2)
+    print("Le ... a choisi la case ",J2," !")
+    for x in range (0,9):
+        if choix[J2]==x:
             choix[J2]="O"
-    else:
-        print("")
-        print("Pas possible. Saisissez un chiffre entre 0 et 8 qui n'est pas déjà pris.")
-        print("")
-        tourJ2()
+            listeJ2.remove(x)
+        
 
 def tour():
-    x=1 #Nombre de tours
-    while x<=9: #Tant qu'il n'y a pas d'égalité
-        if not gagnant(): #S'il n'y a pas de gagnant
-            if x%2==0: #Le nombre de tours sera pair lorsque c'est le tour du joueur 2 et impair lorsque c'est le tour du joueur 1
-                tourJ2()
-            else:
-                tourJ1()
+    tours=1 #Nombre de tours
+    while tours<=9: #Tant qu'il n'y a pas d'égalité
+        if tours%2==0: #Le nombre de tours sera pair lorsque c'est le tour du joueur 2 et impair lorsque c'est le tour du joueur 1
+            tourJ2()
             tableau()
-            x=x+1
-        if x>=10:
-            print("Egalité! Recommencez le programme pour un nouveau jeu.")
+        else:
+            tourJ1()
+                
+        tours=tours+1
+
+        if gagnant()==True:
+            print("Vous avez gagné! :)")
+            time.sleep(2)
+            exit()
+
+        if gagnant()==False:
+            print("Vous avez perdu! :(")
+            time.sleep(2)
+            exit()
+
+    print("Egalité! Recommencez le programme pour un nouveau jeu.")
+    
+        
+        
 
 def gagnant(): #On teste toutes les combinaisons possibles permettant la victoire.
     if choix[0]==choix[1]==choix[2]=="X" or choix[3]==choix[4]==choix[5]=="X" or choix[6]==choix[7]==choix[8]=="X" or choix[0]==choix[3]==choix[6]=="X" or choix[1]==choix[4]==choix[7]=="X" or choix[2]==choix[5]==choix[8]=="X" or choix[0]==choix[4]==choix[8]=="X" or choix[2]==choix[4]==choix[6]=="X": #Pour tester si le joueur 1 a gagné
-        sys.exit(Joueur 1, vous avez gagné! :))
+        return True
     elif choix[0]==choix[1]==choix[2]=="O" or choix[3]==choix[4]==choix[5]=="O" or choix[6]==choix[7]==choix[8]=="O" or choix[0]==choix[3]==choix[6]=="O" or choix[1]==choix[4]==choix[7]=="O" or choix[2]==choix[5]==choix[8]=="O" or choix[0]==choix[4]==choix[8]=="O" or choix[2]==choix[4]==choix[6]=="O": #Pour tester si le joueur 2 a gagné
-        sys.exit("Joueur 2, vous avez gagné! :)")
+        return False
 
 tour()
 gagnant()
